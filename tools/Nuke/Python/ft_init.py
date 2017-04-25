0dc7ef2098082bc6e9133929f42b0c47ae2c7b17
aldmbmtl <aldmbmtl@users.noreply.github.com>
2017-04-25 00:18:04 -0400

    Update ft_init.py

--- tools/Nuke/Python/ft_init.py
+++ tools/Nuke/Python/ft_init.py
@@ -1,3 +1,3 @@
 import FloatingTools
 
-print FloatingTools.cloudImport('aldmbmtl/toolbox', '/tools/Nuke/Python/HatfieldKit.py')
+FloatingTools.cloudImport('aldmbmtl/toolbox', '/tools/Nuke/Python/HatfieldKit.py')
